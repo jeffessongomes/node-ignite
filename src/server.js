e842a91679777bf5cc6c6b026311779bec1b6628
Jeffesson Gomes <jeffessong4@gmail.com>
2023-05-01 18:45:00 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import http from 'http';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
+
+
+const server = http.createServer(async(req, res) => {
+    const { method, url } = req;
+
+    await json(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const {handler} = route;
+        const routeParams = req.url.match(route.path)
+        
+        console.log(extractQueryParams(routeParams.groups.query))
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+
+        req.query = query ? extractQueryParams(routeParams.groups.query) : {}
+
+        return handler(req, res)
+    }
+
+    return res.writeHead(404).end();
+});
+
+server.listen(3333);
